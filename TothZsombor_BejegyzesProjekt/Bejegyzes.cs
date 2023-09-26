71ac955de0227c88e6020b752e9287ea32238b06
tthzsombor <zsombortoooth@gmail.com>
2023-09-26 09:20:07 +0200

    darabszam és bejegyzés bekérése a felhasználótól

--- TothZsombor_BejegyzesProjekt/Bejegyzes.cs
+++ TothZsombor_BejegyzesProjekt/Bejegyzes.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,7 @@ namespace TothZsombor_BejegyzesProjekt
 {
     internal class Bejegyzes
     {
-        private List<Bejegyzes>lista1=new List<Bejegyzes>();
-        private List<Bejegyzes> lista2 = new List<Bejegyzes>();
+       
 
 
         private string szerzo;
@@ -17,6 +18,8 @@ namespace TothZsombor_BejegyzesProjekt
         private int likeok;
         private DateTime letrejott=new DateTime();
         private DateTime szerkesztve=new DateTime();
+        private List<Bejegyzes> bejegyzes;
+        private List<Bejegyzes> beir;
 
         public Bejegyzes(string szerzo, string tartalom)
         {
@@ -58,6 +61,39 @@ namespace TothZsombor_BejegyzesProjekt
             return tostring;
                   
 
+        }
+
+        public void Beker()
+        {
+           
+            Console.Write("Adjon meg egy számot: ");
+            int szam=int.Parse(Console.ReadLine());
+            if (szam%1!=0)
+            {
+                Console.WriteLine("Természetes számot adjon meg");
+            }
+            else
+            {
+                for (int i = 0; i < szam; i++)
+                {
+                    Console.Write("Adja meg a bejegyzés szerzőjét: ");
+                    string szerzo = Console.ReadLine();
+                    Console.Write("Adja meg a bejegyzés tartalmát: ");
+                    string tartalom = Console.ReadLine();
+                    Bejegyzes b1=new Bejegyzes(szerzo, tartalom);
+                    beir.Add(b1);
+                }
+            }
+        }
+
+        private void Beolvas()
+        {
+            StreamReader sr=new StreamReader("bejegyzesek.cvs");
+            while(!sr.EndOfStream)
+            {
+                string sor=sr.ReadLine();
+
+            }
         }
 
 
